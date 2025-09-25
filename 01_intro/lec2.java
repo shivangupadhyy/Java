366d936b5b0bbc0e859fc6e4cdd03037c063ecf7
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-25 10:18:01 +0530

    day 3 java class code update

--- 01_intro/lec2.java
+++ 01_intro/lec2.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class lec2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String w = sc.nextLine();
+        int n  = sc.nextInt();
+        System.out.println("My name is : "+w);
+        System.out.println("multiplaication number is : "+ n * n);
+        sc.close();
         }
 }
